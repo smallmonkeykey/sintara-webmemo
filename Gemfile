f6fb652ef9bdc89fae2efba0e89ed7e27963da6f
smallmonkeykey <sarinitakuoo@gmail.com>
2023-09-06 10:33:31 +0900

    rubocopを設定し、修正

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "sinatra"
-gem "webrick"
+gem 'sinatra'
+gem 'webrick'
+
+# For plain Ruby scripts
+group :development do
+  gem 'rubocop-fjord', require: false
+end
--- main.rb
+++ main.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'json'
 
 set :enviroment, :production
 
 def load_jsonfile
-  File.open("memos.json", "r") do |file|
-    JSON.load(file) || []
+  File.open('memos.json', 'r') do |file|
+    JSON.load_file(file) || []
   end
 end
 
 def write_to_jsonfile(memos_data)
-  File.open("memos.json", "w") do |file|
+  File.open('memos.json', 'w') do |file|
     JSON.dump(memos_data, file)
   end
 end
 
-def give_number_to_memos_data(memos_data, params)
-	ids = memos_data.map {|memo_data| memo_data['id']}
-	ids.max.to_i + 1
+def give_number_to_memos_data(memos_data)
+  ids = memos_data.map { |memo_data| memo_data['id'] }
+  ids.max.to_i + 1
 end
 
 def take_unique_memo(memos_data, params)
-	id_memo_data = {}
+  id_memo_data = {}
 
-	memos_data.each do |memo_data|
-	 id_memo_data = memo_data if memo_data.value?(params[:id].to_i)
-	end
+  memos_data.each do |memo_data|
+    id_memo_data = memo_data if memo_data.value?(params[:id].to_i)
+  end
 
-	id_memo_data
+  id_memo_data
 end
 
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
-	end
+  end
 end
 
 get '/' do
-	redirect '/memos'
+  redirect '/memos'
 end
 
 get '/memos' do
-	@memos_date = load_jsonfile
+  @memos_date = load_jsonfile
 
   erb :top
 end
@@ -51,54 +53,52 @@ get '/memos/create' do
 end
 
 post '/memos/create' do
-	memos_data = load_jsonfile
-	new_memo_id = give_number_to_memos_data(memos_data, params)
-	params['id'] = new_memo_id
-	memos_data << params
-	write_to_jsonfile(memos_data)
+  memos_data = load_jsonfile
+  new_memo_id = give_number_to_memos_data(memos_data)
+  params['id'] = new_memo_id
+  memos_data << params
+  write_to_jsonfile(memos_data)
 
-	redirect '/'
+  redirect '/'
 end
 
 get '/memos/:id/show' do
-	memos_data = load_jsonfile
+  memos_data = load_jsonfile
   @id_memo_data = take_unique_memo(memos_data, params)
 
-	erb :show_memo
+  erb :show_memo
 end
 
 delete '/memos/:id/show' do
-	memos_data = load_jsonfile
-	id_memo_data = take_unique_memo(memos_data, params)
-	memos_data.delete_if{|memo_data| memo_data == id_memo_data }
-	write_to_jsonfile(memos_data)
-	
-	redirect '/'
+  memos_data = load_jsonfile
+  id_memo_data = take_unique_memo(memos_data, params)
+  memos_data.delete_if { |memo_data| memo_data == id_memo_data }
+  write_to_jsonfile(memos_data)
+
+  redirect '/'
 end
 
 get '/memos/:id/edit' do
-	memos_data = load_jsonfile
-	@id_memo_data = take_unique_memo(memos_data, params)
+  memos_data = load_jsonfile
+  @id_memo_data = take_unique_memo(memos_data, params)
 
-	erb :edit_memo
+  erb :edit_memo
 end
 
 patch '/memos/:id/edit' do
-	memos_data = load_jsonfile
-	edit_memo_data = params.delete_if{|key, value|  key == "_method" }
-	edit_memo_data["id"] = edit_memo_data["id"].to_i
+  memos_data = load_jsonfile
+  edit_memo_data = params.delete_if { |key, _value| key == '_method' }
+  edit_memo_data['id'] = edit_memo_data['id'].to_i
 
-	i = 0
-	arry_number = 0
-	memos_data.each do |memo_data|
-		if memo_data.value?(params[:id].to_i)
-			arry_number = i
-		end
-		i =+ 1
-	end
-	
-	memos_data[arry_number] = edit_memo_data
-	write_to_jsonfile(memos_data)
+  i = 0
+  arry_number = 0
+  memos_data.each do |memo_data|
+    arry_number = i if memo_data.value?(params[:id].to_i)
+    i = + 1
+  end
 
-	redirect '/'
+  memos_data[arry_number] = edit_memo_data
+  write_to_jsonfile(memos_data)
+
+  redirect '/'
 end
