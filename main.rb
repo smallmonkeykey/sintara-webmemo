9a64a9d1bb9a08df1041d57856ce71adceb15153
smallmonkeykey <sarinitakuoo@gmail.com>
2023-10-03 13:03:59 +0900

    SQL部分をメソッド化

--- main.rb
+++ main.rb
@@ -16,6 +16,22 @@ def give_number_to_memos(memos)
   memos.map { _1['id'] }.max.to_i + 1
 end
 
+def insert_memo(connection, name, message)
+  connection.exec_params('INSERT INTO memos (name, message) VALUES ($1, $2)', [name, message])
+end
+
+def find_memo(connection, id)
+  connection.exec_params('SELECT * FROM memos WHERE id = $1', [id]).first
+end
+
+def delete_memo(connection, id)
+  connection.exec_params('DELETE FROM Memos WHERE id = $1 ', [id])
+end
+
+def update_memo(connection, name, message, id)
+  connection.exec_params('UPDATE Memos SET name = $1, message = $2 WHERE id = $3', [name, message, id])
+end
+
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
@@ -39,33 +55,28 @@ end
 post '/memos/create' do
   name = params[:name]
   message = params[:message]
-
-  sql = 'INSERT INTO memos (name, message) VALUES ($1, $2)'
-  connection.exec_params(sql, [name, message])
+  insert_memo(connection, name, message)
 
   redirect '/'
 end
 
 get '/memos/:id/show' do
   id = params[:id].to_i
-  sql = 'SELECT * FROM memos WHERE id = $1'
-  @memo = connection.exec_params(sql, [id]).first
+  @memo = find_memo(connection, id)
 
   erb :show_memo
 end
 
 delete '/memos/:id/show' do
   id = params[:id].to_i
-  sql = 'DELETE FROM Memos WHERE id = $1 '
-  connection.exec_params(sql, [id])
-
+  delete_memo(connection, id)
+  
   redirect '/'
 end
 
 get '/memos/:id/edit' do
   id = params[:id].to_i
-  sql = 'SELECT * FROM memos WHERE id = $1'
-  @memo = connection.exec_params(sql, [id]).first
+  @memo = find_memo(connection, id)
 
   erb :edit_memo
 end
@@ -74,9 +85,7 @@ patch '/memos/:id/edit' do
   id = params[:id].to_i
   name = params[:name]
   message = params[:message]
-
-  sql = 'UPDATE Memos SET name = $1, message = $2 WHERE id = $3'
-  connection.exec_params(sql, [name, message, id])
+  update_memo(connection, name, message, id)
 
   redirect '/'
 end
